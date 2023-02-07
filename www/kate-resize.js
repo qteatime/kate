6c60f65c0ea1c09e542b55e65485efeb76e64e70
Q <hello@qteati.me>
2023-02-07 18:01:37 +0100

    Make sure the console fits the screen on mobile

--- www/kate-resize.js
+++ www/kate-resize.js
@@ -4,13 +4,21 @@ void (async function() {
   function on_screen_size() {
     const kw = 1310;
     const iw = window.innerWidth;
+    const ih = window.innerHeight;
+    let zoom = iw/kw;
     if (kw > iw) {
-      kate_body.style.zoom = `${iw/kw}`;
+      kate_body.style.zoom = `${zoom}`;
     } else {
+      zoom = 1;
       kate_body.style.zoom = "1";
     }
+    kate_body.style.left = `${Math.round((iw - kate_body.offsetWidth*zoom) / 2)}px`;
+    kate_body.style.top = `${Math.round((ih - kate_body.offsetHeight*zoom) / 2)}px`;
+    window.scrollTo({ left: 0, top: 0 });
+    document.body.scroll({ left: 0, top: 0 });
   }
 
   window.addEventListener("resize", () => on_screen_size());
   on_screen_size();
+  window.addEventListener("load", () => on_screen_size());
 })();
