8f0010a540ee4f468c3826a952ce57c31bf4b5f3
Q <hello@qteati.me>
2023-04-10 18:28:43 +0200

    Apply simple scene and interactives to media gallery

--- packages/kate-core/source/os/apps/media.ts
+++ packages/kate-core/source/os/apps/media.ts
@@ -3,10 +3,14 @@ import { h } from "../ui/widget";
 import * as UI from "../ui/widget";
 import type { ExtendedInputKey } from "../../kernel";
 import * as Db from "../../data/db";
-import { Scene } from "../ui/scenes";
+import { Scene, SimpleScene } from "../ui/scenes";
 import { SceneViewMedia } from "./view-media";
 
-export class SceneMedia extends Scene {
+export class SceneMedia extends SimpleScene {
+  icon = "images";
+  title = ["Media gallery"];
+  subtitle = h("div", { class: "kate-os-media-status" }, []);
+
   private media = new Map<HTMLElement, Db.Media>();
 
   constructor(
@@ -16,32 +20,19 @@ export class SceneMedia extends Scene {
     super(os);
   }
 
-  render() {
-    return h("div", { class: "kate-os-simple-screen" }, [
-      new UI.Title_bar({
-        left: UI.fragment([
-          UI.fa_icon("images", "lg"),
-          new UI.Section_title(["Media gallery"]),
-        ]),
-        right: h("div", { class: "kate-os-media-status" }, []),
-      }),
-      h("div", { class: "kate-os-scroll" }, [
-        h("div", { class: "kate-os-media-items" }, []),
-      ]),
-      h("div", { class: "kate-os-statusbar" }, [
-        UI.icon_button("x", "Return").on_clicked(this.handle_close),
-        UI.icon_button("o", "View").on_clicked(this.handle_view),
-      ]),
+  body_container(body: UI.Widgetable[]): HTMLElement {
+    return h("div", { class: "kate-os-scroll" }, [
+      h("div", { class: "kate-os-media-items" }, [...body]),
     ]);
   }
 
-  on_attached(): void {
-    this.os.focus_handler.listen(this.canvas, this.handle_key_pressed);
-    this.load_media();
+  body() {
+    return [];
   }
 
-  on_detached(): void {
-    this.os.focus_handler.remove(this.canvas, this.handle_key_pressed);
+  on_attached(): void {
+    super.on_attached();
+    this.load_media();
   }
 
   private async get_media_filtered() {
@@ -66,20 +57,27 @@ export class SceneMedia extends Scene {
     const container = this.canvas.querySelector(".kate-os-media-items")!;
     container.textContent = "";
     for (const [meta, button] of buttons) {
-      const child = UI.render(button)! as HTMLElement;
-      container.append(child);
-      this.media.set(child, meta);
+      container.append(button);
+      this.media.set(button, meta);
     }
   }
 
   private async make_button(x: Db.Media) {
-    const element = new UI.Button([
+    return UI.interactive(
+      this.os,
       h("div", { class: "kate-os-media-thumbnail" }, [
         h("img", { src: x.thumbnail_dataurl }, []),
         this.make_video_length(x.video_length),
       ]),
-    ]).on_clicked(() => this.view(x));
-    return element;
+      [
+        {
+          key: ["o"],
+          label: "View",
+          on_click: true,
+          handler: () => this.view(x),
+        },
+      ]
+    );
   }
 
   private make_video_length(duration: number | null) {
@@ -113,40 +111,6 @@ export class SceneMedia extends Scene {
   private update_status(text: string) {
     this.canvas.querySelector(".kate-os-media-status")!.textContent = text;
   }
-
-  handle_close = () => {
-    this.os.pop_scene();
-  };
-
-  handle_view = () => {
-    const current = this.os.focus_handler.current_focus;
-    if (current != null) {
-      const data = this.media.get(current);
-      if (data != null) {
-        this.view(data);
-      }
-    }
-  };
-
-  handle_key_pressed = (x: { key: ExtendedInputKey; is_repeat: boolean }) => {
-    if (x.is_repeat) {
-      return false;
-    }
-
-    switch (x.key) {
-      case "x": {
-        this.handle_close();
-        return true;
-      }
-
-      case "o": {
-        this.handle_view();
-        return true;
-      }
-    }
-
-    return false;
-  };
 
   view = (x: Db.Media) => {
     const viewer = new SceneViewMedia(this.os, this, x);
