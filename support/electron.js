0df9562049a407d8763441634d234235152f4b93
Quil <robotlolita@users.noreply.github.com>
2023-04-23 18:49:28 +0200

    Support generating pre-built binaries and add more extensive installation instructions. (#3)
    This includes the trade-offs in practicality and security that each
    alternative installation mode offers. In the future Kate will (as much
    as possible) be released as a fully sandbox application, but the
    current pre-built binaries run unsandboxed.
    
    It's likely that future versions will not be based on Electron in order
    to achieve this, but that still leaves some problems with packaging
    for Linux, as sandboxing there doesn't mash well with Chromium's own
    sandboxing requirements.

---
+++ support/electron.js
@@ -0,0 +1,116 @@
+const { exec } = require("./utils");
+const OS = require("os");
+const Path = require("path");
+const FS = require("fs");
+const HTTPS = require("https");
+const Streams = require("stream/promises");
+const Crypto = require("crypto");
+const electron_checksum = require("./electron-checksum");
+
+const version = "v24.1.2";
+
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    HTTPS.get(url, (res) => {
+      if (res.statusCode === 302 || res.statusCode === 301) {
+        fetch(res.headers.location).then(resolve, reject);
+      } else if (res.statusCode === 200) {
+        resolve(res);
+      } else {
+        reject(new Error(`GET ${url} failed with ${res.statusCode}`));
+      }
+    });
+  });
+}
+
+function electron_platform() {
+  switch (process.platform) {
+    case "win32":
+      return "win32";
+    case "linux":
+      return "linux";
+    case "darwin":
+      return "darwin";
+    default:
+      throw new Error(`Not supported ${process.platform}`);
+  }
+}
+
+function electron_arch() {
+  switch (process.arch) {
+    case "arm64":
+      return "arm64";
+    case "x64":
+      return "x64";
+    case "ia32":
+      return "ia32";
+    default:
+      throw new Error(`Not supported ${process.arch}`);
+  }
+}
+
+function make_electron_name(
+  platform = electron_platform(),
+  arch = electron_arch()
+) {
+  return `electron-${version}-${platform}-${arch}.zip`;
+}
+
+async function download_electron(filename, force = false) {
+  const remote_url = `https://github.com/electron/electron/releases/download/${version}/${filename}`;
+  const dest_path = Path.join(__dirname, "..", ".cache", filename);
+  if (FS.existsSync(dest_path) && !force) {
+    console.log("> Using cached", filename);
+    return;
+  }
+
+  console.log("> Downloading", filename);
+  FS.mkdirSync(Path.dirname(dest_path), { recursive: true });
+  const res = await fetch(remote_url);
+  await Streams.pipeline(res, FS.createWriteStream(dest_path));
+  const hash = Crypto.createHash("sha256")
+    .update(FS.readFileSync(dest_path))
+    .digest()
+    .toString("hex");
+  const expected = electron_checksum.get(filename);
+  if (hash !== expected) {
+    throw new Error(
+      `Integrity check failed for ${filename}: expected ${expected}`
+    );
+  }
+}
+
+function unzip_electron(filename, dest) {
+  try {
+    switch (OS.platform()) {
+      case "win32": {
+        exec(
+          `powershell.exe -Command "Expand-Archive .cache/${filename} -DestinationPath ${dest}"`
+        );
+        break;
+      }
+
+      case "linux":
+      case "openbsd":
+      case "freebsd":
+      case "darwin":
+        exec(`unzip .cache/${filename} -d ${dest}`);
+        break;
+
+      default:
+        throw new Error(`: Platform ${OS.platform()} is not supported`);
+    }
+  } catch (_) {
+    console.log(
+      `Could not unzip electron automatically, you should unzip .cache/${filename} in the ${dest} folder.`
+    );
+  }
+}
+
+module.exports = {
+  platform: electron_platform,
+  arch: electron_arch,
+  make_name: make_electron_name,
+  download: download_electron,
+  unzip: unzip_electron,
+};
---
+++ support/utils.js
@@ -0,0 +1,83 @@
+const execSync = require("child_process").execSync;
+const Path = require("path");
+const FS = require("fs");
+const OS = require("os");
+
+function make(target) {
+  const file = Path.join(__dirname, "../make.js");
+  return exec(`node ${file} ${target}`);
+}
+
+function exec(command, opts = {}) {
+  console.log("$>", command);
+  execSync(command, { stdio: ["inherit", "inherit", "inherit"], ...opts });
+}
+
+function copy(from, to) {
+  console.log("-> Copy", from, "->", to);
+  FS.mkdirSync(Path.dirname(to), { recursive: true });
+  FS.copyFileSync(from, to);
+}
+
+function copy_tree(from, to, filter = (x) => true) {
+  console.log("-> Copy tree", from, "->", to);
+
+  const go = (src, dst) => {
+    const stat = FS.statSync(src);
+    if (stat.isFile()) {
+      if (filter(src)) {
+        copy(src, dst);
+      }
+    } else if (stat.isDirectory()) {
+      for (const entry of FS.readdirSync(src)) {
+        go(Path.join(src, entry), Path.join(dst, entry));
+      }
+    } else {
+      throw new Error(`Unsupported entity: ${src}`);
+    }
+  };
+
+  go(from, to);
+}
+
+function assert_root(what) {
+  if (Path.resolve(process.cwd()) !== Path.resolve(__dirname, "..")) {
+    throw new Error(`${what} must be run from the root of the repository`);
+  }
+}
+
+function zip(src, dst) {
+  if (OS.platform() !== "win32") {
+    throw new Error(`Only supported on windows`);
+  }
+  exec(
+    `powershell.exe -Command "Compress-Archive -Path '${src}' -DestinationPath '${dst}' -CompressionLevel Optimal"`
+  );
+}
+
+function tar(src, dst) {
+  if (!["linux", "openbsd", "freebsd", "darwin"].includes(OS.platform())) {
+    throw new Error(`Only supported on POSIX`);
+  }
+
+  exec(`tar -czf '../${Path.basename(dst)}' *`, { cwd: Path.resolve(src) });
+}
+
+function pack(src) {
+  if (OS.platform() === "win32") {
+    zip(src, `${src}.zip`);
+  } else {
+    tar(src, `${src}.tar.gz`);
+  }
+}
+
+module.exports = {
+  copy,
+  copy_tree,
+  exec,
+  make,
+  assert_root,
+  zip,
+  tar,
+  pack,
+};
