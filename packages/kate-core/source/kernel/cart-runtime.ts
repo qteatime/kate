9d3b5ce4c5f4a2846fd792e4deccdde24772a50e
Q <hello@qteati.me>
2023-05-20 19:45:35 +0200

    Delegate autoplay

--- packages/kate-core/source/kernel/cart-runtime.ts
+++ packages/kate-core/source/kernel/cart-runtime.ts
@@ -157,7 +157,7 @@ export class CR_Web_archive extends CartRuntime {
 
     frame.className = "kate-game-frame kate-game-frame-defaults";
     (frame as any).sandbox = "allow-scripts";
-    frame.allow = "";
+    frame.allow = "autoplay";
     (frame as any).csp =
       "default-src data: blob: 'unsafe-inline' 'unsafe-eval' 'unsafe-inline' 'wasm-unsafe-eval'; navigate-to 'none'";
 
