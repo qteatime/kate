ce2309f730d893ebec7ea0480195e86b813744f3
Q <hello@qteati.me>
2023-10-14 12:48:09 +0200

    Place tests in public domain

--- tests/unit/kernel/clock.spec.ts
+++ tests/unit/kernel/clock.spec.ts
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
 import test, { JSHandle } from "playwright/test";
 import { load } from "../unit";
 import type { KateConsoleClock } from "../../../packages/kate-core/source/kernel";
--- tests/unit/kernel/resource.spec.ts
+++ tests/unit/kernel/resource.spec.ts
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
 import test, { expect } from "playwright/test";
 import { load } from "../unit";
 
