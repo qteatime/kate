6cee738cba385b237764f650c317efa4ee1ed259
Q <hello@qteati.me>
2023-12-09 14:42:52 +0100

    wip: add missing headers

--- packages/kate-core/source/kernel/process/runtimes.ts
+++ packages/kate-core/source/kernel/process/runtimes.ts
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import * as Cart from "../../cart";
 import { make_id } from "../../utils";
 import { VirtualConsole } from "../virtual";
